212e9ae238beb8b3213b370fcc76adc4537511e1
Michal Hanebach <michal.hanebach@gmail.com>
2018-07-11 19:07:31 +0200

    add shortest path search algorithm tests

--- tests/GraphTest.java
+++ tests/GraphTest.java
@@ -2,6 +2,9 @@ import graph.Graph;
 import graph.Node;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,7 +14,74 @@ import static org.junit.Assert.*;
 public class GraphTest {
 
     @Test
-    public void BFS() {
+    public void testShortestPathUnweighted() {
+
+        Node nodeA = new Node("A");
+        Node nodeB = new Node("B");
+        Node nodeC = new Node("C");
+        Node nodeD = new Node("D");
+        Node nodeE = new Node("E");
+        Node nodeF = new Node("F");
+        Node nodeG = new Node("G");
+        Node nodeH = new Node("H");
+
+        Graph graph = new Graph();
+        graph.addNode(nodeA);
+        graph.addNode(nodeB);
+        graph.addNode(nodeC);
+        graph.addNode(nodeD);
+        graph.addNode(nodeE);
+        graph.addNode(nodeF);
+        graph.addNode(nodeG);
+        graph.addNode(nodeH);
+
+        nodeA.addNeighbor(nodeB);
+        nodeA.addNeighbor(nodeD);
+
+        nodeB.addNeighbor(nodeA);
+        nodeB.addNeighbor(nodeC);
+
+        nodeC.addNeighbor(nodeB);
+
+        nodeD.addNeighbor(nodeA);
+        nodeD.addNeighbor(nodeE);
+        nodeD.addNeighbor(nodeH);
+
+        nodeE.addNeighbor(nodeD);
+        nodeE.addNeighbor(nodeF);
+//        nodeE.addNeighbor(nodeG);
+        nodeE.addNeighbor(nodeH);
+
+        nodeF.addNeighbor(nodeE);
+        nodeF.addNeighbor(nodeG);
+
+//        nodeG.addNeighbor(nodeE);
+        nodeG.addNeighbor(nodeF);
+        nodeG.addNeighbor(nodeH);
+
+        nodeH.addNeighbor(nodeD);
+        nodeH.addNeighbor(nodeE);
+        nodeH.addNeighbor(nodeG);
+
+        List<Node> actual = graph.shortestPathUnweighted(nodeC, nodeH);
+        assertEquals(Arrays.asList(nodeC, nodeB, nodeA, nodeD, nodeH), actual);
+
+        actual = graph.shortestPathUnweighted(nodeF, nodeD);
+        assertEquals(Arrays.asList(nodeF, nodeE, nodeD), actual);
+
+        actual = graph.shortestPathUnweighted(nodeC, nodeG);
+        assertEquals(Arrays.asList(nodeC, nodeB, nodeA, nodeD, nodeH, nodeG), actual);
+
+        Node dummyNode = new Node("node which is not in the graph");
+        actual = graph.shortestPathUnweighted(nodeC, dummyNode);
+        assertEquals(0, actual.size());
+
+        actual = graph.shortestPathUnweighted(dummyNode, nodeC);
+        assertEquals(0, actual.size());
+    }
+
+    @Test
+    public void testBFS() {
 
         Node nodeA = new Node("A");
         Node nodeB = new Node("B");
