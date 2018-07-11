9d681f14f604036c711efc1705ddd5690c7d1dd3
SebastianHalinski <sebastian.halinski@gmail.com>
2018-07-11 10:24:41 +0200

    Create full graph creator class

--- src/application/GraphCreator.java
+++ src/application/GraphCreator.java
@@ -2,29 +2,88 @@ import graph.Graph;
 import graph.Node;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class GraphCreator{
+class GraphCreator{
+    private ArrayList<Node> nodeList = new ArrayList<>();
 
-    Node node;
-    Graph graph;
+    Graph runGraphCreator(){
+        String path = "graph.txt";
+        createNode(path);
+        createNeighbor(path);
+        Graph graph = buildGraph();
+        printNodeData(graph);
+        return graph;
+    }
 
-    public void createNode(String path){
+
+    private void createNode(String path){
+
+        String[] words;
         try{
             BufferedReader br = new BufferedReader(new FileReader(path));
-            String line = br.readLine();
-            if (br.readLine() == null){
-                return;
-            }
-            while ((line != null)){
-                
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            String line;
+
+            while (((line=br.readLine()) != null)){
+                words = line.split(" ");
+                String nodeData = words[0];
+                Node node = new Node(nodeData);
+                nodeList.add(node);
+                }
+            br.close();
+
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+    }
+    private void createNeighbor(String path){
+        String[] words;
+        int nodeIndex = 0;
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line;
+
+            while (((line=br.readLine()) != null)){
+
+                words = line.split(" ");
+                for (int i = 1; i < words.length; i++){
+                    for(Node node : nodeList){
+                        if(node.getData().equals(words[i])){
+                            nodeList.get(nodeIndex).addNeighbor(node);
+                            System.out.println(node.getData()+" is Neighbor " + nodeList.get(nodeIndex).getData());
+                        }
+                    }
+                }
+                nodeIndex ++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Graph buildGraph(){
+        Graph graph = new Graph();
+        for (Node node: nodeList){
+            graph.addNode(node);
+        }
+        return graph;
+    }
+
+    private void printNodeData(Graph graph){
+        for(int i = 0; i < nodeList.size(); i++){
+            Node node = graph.getNodeByName(nodeList.get(i).getData());
+            System.out.print(node.getData() + " --> siez: ");
+            System.out.println(nodeList.get(i).getNeighbors().size() + " contains: ");
+            List<Node> list = node.getNeighbors();
+            for(Node l : list){
+                System.out.print(l.getData());
+            }
+            System.out.println();
+
         }
     }
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,9 @@
-package application;
+import graph.Graph;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        GraphCreator grc = new GraphCreator();
+        grc.runGraphCreator();
     }
 }
